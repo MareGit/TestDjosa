f0b8357e139c44eaf89030aa5e81ac129bd84d1c
MareGit <zokasecer@gmail.com>
2025-05-15 22:49:07 +0200

    First commit

---
+++ Tables/dbo.Table_2.sql
@@ -0,0 +1,8 @@
+﻿SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Table_2](
+	[dffefefe] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[veweveve] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
