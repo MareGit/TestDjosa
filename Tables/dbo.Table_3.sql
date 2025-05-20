cbdd0bf761218e61ae920efc07e0de245fef7df4
MareGit <zokasecer@gmail.com>
2025-05-20 20:36:04 +0200

    Delete Tables directory

--- Tables/dbo.Table_3.sql
+++
@@ -1,9 +0,0 @@
-﻿SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Table_3](
-	[fvfvfvfvfvfv] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[cdcddvvrv] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[rgregrgrg] [date] NULL
-) ON [PRIMARY]
