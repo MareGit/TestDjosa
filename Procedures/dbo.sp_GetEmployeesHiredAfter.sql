39b52056350b6081b049a66b54a3dcd5e60f6757
MareGit <zokasecer@gmail.com>
2025-08-04 22:43:47 +0200

    jj

---
+++ Procedures/dbo.sp_GetEmployeesHiredAfter.sql
@@ -0,0 +1,14 @@
+﻿SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- Create stored procedure
+CREATE PROCEDURE [dbo].[sp_GetEmployeesHiredAfter]
+    @HireDate DATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+    SELECT EmployeeID, FirstName, LastNameg, HireDate5
+    FROM Employees
+ 
+END;
