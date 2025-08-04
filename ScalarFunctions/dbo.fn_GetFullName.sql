f473d7ecc0203d8b3782eee62f284bc018949fd3
MareGit <zokasecer@gmail.com>
2025-08-04 22:36:51 +0200

    thhhgh

---
+++ ScalarFunctions/dbo.fn_GetFullName.sql
@@ -0,0 +1,15 @@
+﻿SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- Create scalar function
+CREATE FUNCTION [dbo].[fn_GetFullName](@EmployeeID INT)
+RETURNS NVARCHAR(120)
+AS
+BEGIN
+    DECLARE @FullName NVARCHAR(120);
+    SELECT @FullName = FirstName
+    FROM Employees
+    WHERE EmployeeID = @EmployeeID;
+    RETURN @FullName;
+END;
