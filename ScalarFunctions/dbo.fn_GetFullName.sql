dabb90617c0ddc9f1b8d68e1dfd56c973533dd9e
MareGit <zokasecer@gmail.com>
2025-08-04 22:42:44 +0200

    Staged

--- ScalarFunctions/dbo.fn_GetFullName.sql
+++ ScalarFunctions/dbo.fn_GetFullName.sql
@@ -7,7 +7,7 @@ CREATE FUNCTION [dbo].[fn_GetFullName](@EmployeeID INT)
 RETURNS NVARCHAR(120)
 AS
 BEGIN
-    DECLARE @FullName NVARCHAR(120);
+    DECLARE @FullName NVARCHAR(125);
     SELECT @FullName = FirstName
     FROM Employees
     WHERE EmployeeID = @EmployeeID;
