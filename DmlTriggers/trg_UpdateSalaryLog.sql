d63565671fefbdb4c03797ab3faf6df89266d302
MareGit <zokasecer@gmail.com>
2025-08-04 22:34:39 +0200

    ss

---
+++ DmlTriggers/trg_UpdateSalaryLog.sql
@@ -0,0 +1,19 @@
+﻿SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- Create trigger
+CREATE TRIGGER [dbo].[trg_UpdateSalaryLog]
+ON [dbo].[Employees]
+AFTER UPDATE
+AS
+BEGIN
+    IF UPDATE(Salary)
+    BEGIN
+        PRINT 'Salary updateed for an employee.';
+    END
+END;
+
+GO
+ALTER TABLE [dbo].[Employees] ENABLE TRIGGER [trg_UpdateSalaryLog]
