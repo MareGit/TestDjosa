f023922965135d95086ea9baf49558ce8e1ff439
MareGit <zokasecer@gmail.com>
2025-08-04 22:17:39 +0200

    first

---
+++ Procedures/dbo.sp_GetEmployeesHiredAfter.sql
@@ -0,0 +1,14 @@
+﻿SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- Create stored procedure
+CREATE PROCEDURE sp_GetEmployeesHiredAfter
+    @HireDate DATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+    SELECT EmployeeID, FirstName, LastName, HireDate, Salary
+    FROM Employees
+    WHERE HireDate > @HireDate;
+END;
---
+++ ScalarFunctions/dbo.fn_GetFullName.sql
@@ -0,0 +1,15 @@
+﻿SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- Create scalar function
+CREATE FUNCTION fn_GetFullName(@EmployeeID INT)
+RETURNS NVARCHAR(120)
+AS
+BEGIN
+    DECLARE @FullName NVARCHAR(120);
+    SELECT @FullName = FirstName + ' ' + LastName
+    FROM Employees
+    WHERE EmployeeID = @EmployeeID;
+    RETURN @FullName;
+END;
