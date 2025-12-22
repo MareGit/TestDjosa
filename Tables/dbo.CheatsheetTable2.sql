1496de31f323d25f0b65d0ea3a71a6b40b598b2e
MareGit <zokasecer@gmail.com>
2025-12-22 23:22:21 +0100

    dgdgd

--- Tables/dbo.CheatsheetTable2.sql
+++ Tables/dbo.CheatsheetTable2.sql
@@ -2,7 +2,7 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[CheatsheetTable] (
+CREATE TABLE [dbo].[CheatsheetTable2] (
     [ID]        INT             NULL,
     [FirstName] NVARCHAR (50)   COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
     [LastName]  NVARCHAR (50)   COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
@@ -16,5 +16,5 @@ CREATE TABLE [dbo].[CheatsheetTable] (
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY];
 GO
 ALTER AUTHORIZATION
-    ON [dbo].[CheatsheetTable]
+    ON [dbo].[CheatsheetTable2]
     TO SCHEMA OWNER;
