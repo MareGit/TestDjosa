060e794b1b534b753cc09e167f716e54dcbbe088
MareGit <zokasecer@gmail.com>
2026-01-05 23:51:58 +0100

    Update dbo.Table1.sql

--- Tables/dbo.Table1.sql
+++ Tables/dbo.Table1.sql
@@ -5,7 +5,7 @@ GO
 CREATE TABLE [dbo].[Table1] (
     [ID]           INT             IDENTITY (1, 1) NOT NULL,
     [Name]         NVARCHAR (50)   COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-    [Valueg]       DECIMAL (10, 2) NULL,
+    [Valuegu]       DECIMAL (10, 2) NULL,
     [CreatedDate5] DATETIME        NULL,
     [Value]        DECIMAL (10, 2) NULL,
     PRIMARY KEY CLUSTERED ([ID] ASC) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
